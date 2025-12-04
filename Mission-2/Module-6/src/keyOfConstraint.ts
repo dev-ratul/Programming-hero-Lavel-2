cff9b1d13cbf9825385b10a41cdfe0fca0172c90
syz-bash <syzz.bd@gmail.com>
2025-12-05 00:33:20 +0600

    l

--- Mission-2/Module-6/src/keyOfConstraint.ts
+++ Mission-2/Module-6/src/keyOfConstraint.ts
@@ -6,3 +6,29 @@ type user1={
 
 type result1= "name"|"id";
 type user3 = keyof user1;
+
+type User= {
+    name: string;
+    id: number;
+    address: {
+        city: string;
+    }
+}
+
+
+const user: User= {
+    name: "ratul",
+    id: 4343,
+    address: {
+        city: "tangail"
+    }
+}
+
+// console.log(user['address']['city'])
+
+const userData= (obj: User, value: keyof User)=>{
+    return obj[value];
+}
+
+const result = userData(user, "id")
+console.log(result)
